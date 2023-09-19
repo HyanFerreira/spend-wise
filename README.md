35a964d6643cfcd0ef2204220d8b321454eff8b9
Hyan Ferreira <hcgamesmg2016@gmail.com>
2023-09-19 10:14:27 -0300

    push - 19/09

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projeto Gerenciador Financeiro
+**Autores:** Hyan Ferreira, Emerson Soares e Gabriel Souza
+
+### Descrição do projeto
+Gerenciador Financeiro SpendWise.
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SignUp() {
+    return (
+        <view>
+            <Text>
+                Tela de cadastro
+            </Text>
+        </view>
+    );
+}
