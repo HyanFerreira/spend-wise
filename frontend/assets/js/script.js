1f132369ce0dfd79e2b271efc64819a4ad1f9726
Hyan Ferreira <114369661+HyanFerreira@users.noreply.github.com>
2023-10-13 22:28:30 -0300

    password visibility js - 13/10

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -7,7 +7,7 @@ let contentSignUp = document.querySelector('.content-signup-welcome');
 
 btnCadastrar.addEventListener('click', (e) => {
     e.preventDefault();
-    
+
     contentCadastrar.style.transform = "translateX(0)";
     contentCadastrar.style.opacity = "1"
     contentSignUp.style.transform = "translateX(100%)";
@@ -29,4 +29,32 @@ btnLogin.addEventListener('click', (e) => {
     contentLogin.style.opacity = "1"
     contentSignIn.style.transform = "translateX(-100%)";
     contentSignIn.style.opacity = "0";
+});
+
+
+
+const passwordFieldSignIn = document.getElementById('password-signin');
+const showPasswordLockSignIn = document.querySelector('.password-lock');
+
+showPasswordLockSignIn.addEventListener('click', () => {
+    if (passwordFieldSignIn.type === 'password') {
+        passwordFieldSignIn.type = 'text';
+        showPasswordLockSignIn.src = './assets/img/password-unlock.svg';
+    } else {
+        passwordFieldSignIn.type = 'password';
+        showPasswordLockSignIn.src = './assets/img/password-lock.svg';
+    }
+});
+
+const passwordFieldSignUp = document.getElementById('password-signup');
+const showPasswordLockSignUp = document.querySelector('.password-lock-signup');
+
+showPasswordLockSignUp.addEventListener('click', () => {
+    if (passwordFieldSignUp.type === 'password') {
+        passwordFieldSignUp.type = 'text';
+        showPasswordLockSignUp.src = './assets/img/password-unlock.svg';
+    } else {
+        passwordFieldSignUp.type = 'password';
+        showPasswordLockSignUp.src = './assets/img/password-lock.svg';
+    }
 });
